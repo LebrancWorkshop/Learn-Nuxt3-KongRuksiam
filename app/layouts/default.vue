2f492c8d5470011201a6c3ab41aa64d8abb404c8
lebrancconvas <lebrancconvas@gmail.com>
2023-08-14 03:13:22 +0700

    ✨ feat: update web layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <slot />
+    </div>
+    <footer>
+      <p>Copyright 2023, Made by <a href="https://github.com/lebrancconvas">Poom Yimyuean (@lebrancconvas)</a></p>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  footer {
+    position: fixed;
+    height: 50px;
+    width: 100%;
+    text-align: center;
+    left: 0;
+    bottom: 0
+  }
+</style>
