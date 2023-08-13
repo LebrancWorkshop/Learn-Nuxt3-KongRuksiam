b6009e72ac7d1be99ff3c1170dba0ccf8c9cbf2b
lebrancconvas <lebrancconvas@gmail.com>
2023-08-14 00:28:30 +0700

    🚚 refactor: clear starter code

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <h1>Nuxt3 Tutorial by Kong Ruksiam</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
